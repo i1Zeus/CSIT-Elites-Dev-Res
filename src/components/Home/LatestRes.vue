b5648a28783740d8f6d8064e225f99d08cbc0e4b
Mosa Nameer <mosa.mn777@gmail.com>
2022-07-14 01:28:31 +0300

    add

--- src/components/Home/LatestRes.vue
+++ src/components/Home/LatestRes.vue
@@ -3,18 +3,18 @@
 
 <template>
   <div class="flex ">
-    <div class="bg-white w-full flex justify-around mt-5">
-        <div>
+    <div class="bg-white w-full flex justify-around mt-5 m-10">
+        <div class="text-black text-lg font-semibold">
             Latest Res.
         </div>
-        <div>
+        <div class="text-green-500 font-semibold">
             <u><a href="">See more></a></u>
         </div>
     </div>
 
 
-    <div class="">
-    
+    <div class="bg-gray-200 grid-cols-2">
+
     </div>
   </div>
 </template>
