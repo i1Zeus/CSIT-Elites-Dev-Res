bdfcbd2245cc2346ef71138752d2addceeddbd64
Hussein Najah <husseinnajah123@gmail.com>
2022-07-13 22:02:50 +0300

    Font Awsome

--- src/main.js
+++ src/main.js
@@ -5,10 +5,18 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
-import './assets/tailwind.css'
+import "./assets/tailwind.css";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+/* Here We Add FontAwsome Icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+/* Remember to mention the newest icon in the Library */
+library.add(faUserSecret);
 
 const app = createApp(App);
-
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(createPinia());
 app.use(router);
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
+    <font-awesome-icon icon="fa-solid fa-user-secret" />
     <h1 class="bg-white">This is an about page</h1>
   </div>
 </template>
