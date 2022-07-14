23361d2d640f243d4cb1a48e7bcdcdaa5be278f0
Mosa Nameer <mosa.mn777@gmail.com>
2022-07-14 15:27:35 +0300

    add

--- src/components/Home/LatestRes.vue
+++ src/components/Home/LatestRes.vue
@@ -3,29 +3,29 @@
 
 <template>
   <div class="flex-col">
-    <div class="bg-white flex justify-between mt-5 mx-36">
+    <div class="bg-white flex justify-between mt-5 mx-32">
       <div class="text-black text-lg font-semibold">Latest Res.</div>
       <div class="text-green-500 font-semibold">
         <u><a href="">See more></a></u>
       </div>
     </div>
 
-    <div class="bg-gray-100 rounded-2xl grid grid-cols-2 mx-36 my-10">
+    <div class="bg-gray-100 rounded-2xl grid grid-cols-2 mx-32 my-10">
 
       <div class="bg-white rounded-2xl grid grid-cols-12 m-4 h-32">
         <div class="col-span-4  ">
             <img class="h-full w-full rounded-2xl object-cover p-3" src="https://www.aspfaqs.com/wp-content/uploads/2021/10/pankaj-patel-_SgRNwAVNKw-unsplash-1.jpg" alt="">
         </div>
-        <div class="col-span-7 mx-3">
+        <div class="col-span-8 mx-3">
             <p class="mt-3 mr-3 font-semibold">How to hack Hassan K. in one min</p>
             <p class="text-gray-400">Jawi & Manar</p>
             <p class="text-gray-400 ">23/11/2022</p>
         </div>
-        <div>
+        <!-- <div>
             <p class="col-span-1 text-green-500 mt-12">
                 <font-awesome-icon icon="fa-solid fa-angle-right" />
             </p>
-        </div>
+        </div> -->
       </div>
 
 
