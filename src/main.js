bdf252dbdd66e36601d88c92efd75e33a1b6ebe2
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-07-14 15:15:22 +0300

    Revert "Revert "Revert "Merge remote-tracking branch 'origin/Hussein' into fatma"""
    This reverts commit 27b53f4fbbaae844a3ca194fa57b3b8e91ac0794.

--- src/main.js
+++ src/main.js
@@ -9,14 +9,11 @@ import "./assets/tailwind.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-/* =======> Here We Add FontAwsome Icons <======= */
+/* Here We Add FontAwsome Icons */
 import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
-import { faBookOpenReader } from "@fortawesome/free-solid-svg-icons";
-import { faUsers } from "@fortawesome/free-solid-svg-icons";
 
-/* =======> Remember to mention the newest icon in the Library <======= */
-library.add(faUserSecret, faBookOpenReader, faUsers);
+/* Remember to mention the newest icon in the Library */
+library.add(faUserSecret);
 
 const app = createApp(App);
 app.component("font-awesome-icon", FontAwesomeIcon);
