64112a67fbd1523eff8bb9808a6cb6ae4c585abd
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-07-14 03:58:51 +0300

    add

---
+++ src/components/Home/CategoryCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-72 bg-white rounded-lg border border-secondary-300 shadow-md">
+        <a href="#">
+            <img class="rounded-lg w-72 h-36"
+                src="https://en.number13.de/content/images/2021/08/photo-1542831371-29b0f74f9713-2.jpg" alt="">
+        </a>
+        <div class="p-2">
+            <h3 class="font-semibold">Frontend developer</h3>
+
+            <div class="grid grid-cols-2">
+                <p class="text-gray-400 grid-col-1">5 Sub</p>
+                <p class="text-gray-400 grid-col-1">10 Ref.</p>
+
+                <!-- <a href="#" class="grid-col-1">
+                    <i class="fa-solid fa-angle-right"></i>
+                </a> -->
+            </div>
+        </div>
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import Category from "../components/Home/CategoryCard.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,13 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
+
+    {
+      path: "/category",
+      name: "category",
+      component: Category,
+    },
+
   ],
 });
 
