39ce2380438c6e9f7b8427c85dc8e2fe99308c29
Mosa Nameer <mosa.mn777@gmail.com>
2022-07-14 20:56:58 +0300

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-
+import ResShow from "../views/ResShow.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
+    
+    {path: "/", name: "home", component: HomeView },
+    {path: "/Resorces", name: "ResShow", component: ResShow},
   ],
 });
 
