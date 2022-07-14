17adfe73d62b45c85f0d92583a8344746b90f4d7
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-07-14 15:13:02 +0300

    Revert "Merge remote-tracking branch 'origin/Hussein' into fatma"
    This reverts commit 13aca081ef57eb885efa687befe6337704c10bc5, reversing
    changes made to f277ff42333c15e333a0a8383a1da8eaa1344ce2.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
-  <div>
+  <div class="mx-10">
+    <h1>Home</h1>
     <MainComp />
-    <CategoryCard />
-    <LatestRes />
+    <CategoryShow />
+    <LatestRes/>
+
   </div>
+  
 </template>
 
 <script>
-import MainComp from "@/components/Home/MainComp.vue";
-import LatestRes from "@/components/Home/LatestRes.vue";
-import CategoryCard from "@/components/Home/CategoryCard.vue";
+import MainComp from "../components/Home/MainComp.vue";
+import LatestRes from '@/components/Home/LatestRes.vue';
+import CategoryShow from '@/components/Category/CategoryShow.vue';
 
 export default {
-  components: { MainComp, LatestRes, CategoryCard },
+  components: { MainComp, LatestRes, CategoryShow },
 };
 </script>
