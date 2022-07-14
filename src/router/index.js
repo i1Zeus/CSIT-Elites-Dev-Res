d5a9b2db67514351446395cce3344e34404dd8d4
Hussein Najah <husseinnajah123@gmail.com>
2022-07-14 10:33:49 +0300

    Revert "Merge remote-tracking branch 'origin/Fatma' into Mosa"
    This reverts commit 5e0e2bdb493c93439bf8c887e4742a81d262c786, reversing
    changes made to e0f70251881c7539d416dd77afe49b77cbac1fb5.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import Category from "../components/Home/CategoryCard.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +9,6 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
-
-    {
-      path: "/category",
-      name: "category",
-      component: Category,
-    },
-
   ],
 });
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,17 @@
 <template>
-  <div class="mx-10">
-    <h1>Home</h1>
-    <LatestRes></LatestRes>
-    <MainComp />
-
-    <!-- ========================= Category Section ========================= -->
-    <div class="grid grid-cols-2">
-      <h1 class="text-left font-semibold mt-5 text-xl">Category</h1>
-      <a href="" class="pt-6 text-right mr-3 text-primary-500">
-        see more
-        <i class="fa-solid fa-angle-right"></i>
-      </a>
-    </div>
-    <div class="grid grid-cols-3 bg-gray-100 rounded-lg mt-2 place-items-center py-5">
-      <CategoryCard class="grid-cols-1" />
-      <CategoryCard class="grid-cols-1" />
-      <CategoryCard class="grid-cols-1" />
-    </div>
-    <!-- ========================= End Category Section ========================= -->
-
-  </div>
+<div>
+  <LatestRes/>
+</div>
 </template>
 
-<script>
-import MainComp from "../components/Home/MainComp.vue";
-import LatestRes from '@/components/Home/LatestRes.vue';
-import CategoryCard from '@/components/Home/CategoryCard.vue';
 
-export default {
-  components: { MainComp, LatestRes, CategoryCard },
-};
+
+
+<script>
+import LatestRes from '../components/Home/LatestRes.vue';
+
+export default{
+    components: {LatestRes}
+}
 </script>
 
