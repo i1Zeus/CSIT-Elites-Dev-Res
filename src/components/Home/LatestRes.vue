e0f70251881c7539d416dd77afe49b77cbac1fb5
Mosa Nameer <mosa.mn777@gmail.com>
2022-07-14 03:46:15 +0300

    add

--- src/components/Home/LatestRes.vue
+++ src/components/Home/LatestRes.vue
@@ -2,18 +2,149 @@
 </script>
 
 <template>
-  <div class="flex ">
-    <div class="bg-white w-full flex justify-around mt-5 m-10">
-        <div class="text-black text-lg font-semibold">
-            Latest Res.
-        </div>
-        <div class="text-green-500 font-semibold">
-            <u><a href="">See more></a></u>
-        </div>
+  <div class="flex-col">
+    <div class="bg-white flex justify-between mt-5 mx-36">
+      <div class="text-black text-lg font-semibold">Latest Res.</div>
+      <div class="text-green-500 font-semibold">
+        <u><a href="">See more></a></u>
+      </div>
     </div>
 
+    <div class="bg-gray-100 rounded-2xl grid grid-cols-2 mx-36 my-10">
 
-    <div class="bg-gray-200 grid-cols-2">
+      <div class="bg-white rounded-2xl grid grid-cols-12 m-4 h-32">
+        <div class="col-span-4  ">
+            <img class="h-full w-full rounded-2xl object-cover p-3" src="https://www.aspfaqs.com/wp-content/uploads/2021/10/pankaj-patel-_SgRNwAVNKw-unsplash-1.jpg" alt="">
+        </div>
+        <div class="col-span-7 mx-3">
+            <p class="mt-3 mr-3 font-semibold">How to hack Hassan K. in one min</p>
+            <p class="text-gray-400">Jawi & Manar</p>
+            <p class="text-gray-400 ">23/11/2022</p>
+        </div>
+        <div>
+            <p class="col-span-1 text-green-500 mt-12">
+                <font-awesome-icon icon="fa-solid fa-angle-right" />
+            </p>
+        </div>
+      </div>
+
+
+
+
+      <div class="bg-white rounded-2xl grid grid-cols-12 m-4 h-32">
+        <div class="col-span-4  ">
+            <img class="h-full w-full rounded-2xl object-cover p-3" src="https://www.aspfaqs.com/wp-content/uploads/2021/10/pankaj-patel-_SgRNwAVNKw-unsplash-1.jpg" alt="">
+        </div>
+        <div class="col-span-7 mx-3">
+            <p class="mt-3 mr-3 font-semibold">How to hack Hassan K. in one min</p>
+            <p class="text-gray-400">Jawi & Manar</p>
+            <p class="text-gray-400 ">23/11/2022</p>
+        </div>
+        <div>
+            <p class="col-span-1 text-green-500 mt-12">
+                <font-awesome-icon icon="fa-solid fa-angle-right" />
+            </p>
+        </div>
+      </div>
+
+
+
+
+      <div class="bg-white rounded-2xl grid grid-cols-12 m-4 h-32">
+        <div class="col-span-4  ">
+            <img class="h-full w-full rounded-2xl object-cover p-3" src="https://www.aspfaqs.com/wp-content/uploads/2021/10/pankaj-patel-_SgRNwAVNKw-unsplash-1.jpg" alt="">
+        </div>
+        <div class="col-span-7 mx-3">
+            <p class="mt-3 mr-3 font-semibold">How to hack Hassan K. in one min</p>
+            <p class="text-gray-400">Jawi & Manar</p>
+            <p class="text-gray-400 ">23/11/2022</p>
+        </div>
+        <div>
+            <p class="col-span-1 text-green-500 mt-12">
+                <font-awesome-icon icon="fa-solid fa-angle-right" />
+            </p>
+        </div>
+      </div>
+
+
+
+
+      <div class="bg-white rounded-2xl grid grid-cols-12 m-4 h-32">
+        <div class="col-span-4  ">
+            <img class="h-full w-full rounded-2xl object-cover p-3" src="https://www.aspfaqs.com/wp-content/uploads/2021/10/pankaj-patel-_SgRNwAVNKw-unsplash-1.jpg" alt="">
+        </div>
+        <div class="col-span-7 mx-3">
+            <p class="mt-3 mr-3 font-semibold">How to hack Hassan K. in one min</p>
+            <p class="text-gray-400">Jawi & Manar</p>
+            <p class="text-gray-400 ">23/11/2022</p>
+        </div>
+        <div>
+            <p class="col-span-1 text-green-500 mt-12">
+                <font-awesome-icon icon="fa-solid fa-angle-right" />
+            </p>
+        </div>
+      </div>
+
+
+
+
+      <div class="bg-white rounded-2xl grid grid-cols-12 m-4 h-32">
+        <div class="col-span-4  ">
+            <img class="h-full w-full rounded-2xl object-cover p-3" src="https://www.aspfaqs.com/wp-content/uploads/2021/10/pankaj-patel-_SgRNwAVNKw-unsplash-1.jpg" alt="">
+        </div>
+        <div class="col-span-7 mx-3">
+            <p class="mt-3 mr-3 font-semibold">How to hack Hassan K. in one min</p>
+            <p class="text-gray-400">Jawi & Manar</p>
+            <p class="text-gray-400 ">23/11/2022</p>
+        </div>
+        <div>
+            <p class="col-span-1 text-green-500 mt-12">
+                <font-awesome-icon icon="fa-solid fa-angle-right" />
+            </p>
+        </div>
+      </div>
+
+
+
+
+      <div class="bg-white rounded-2xl grid grid-cols-12 m-4 h-32">
+        <div class="col-span-4  ">
+            <img class="h-full w-full rounded-2xl object-cover p-3" src="https://www.aspfaqs.com/wp-content/uploads/2021/10/pankaj-patel-_SgRNwAVNKw-unsplash-1.jpg" alt="">
+        </div>
+        <div class="col-span-7 mx-3">
+            <p class="mt-3 mr-3 font-semibold">How to hack Hassan K. in one min</p>
+            <p class="text-gray-400">Jawi & Manar</p>
+            <p class="text-gray-400 ">23/11/2022</p>
+        </div>
+        <div>
+            <p class="col-span-1 text-green-500 mt-12">
+                <font-awesome-icon icon="fa-solid fa-angle-right" />
+            </p>
+        </div>
+      </div>
+
+
+
+
+     
+
+
+<!-- <div class="grid grid-cols-12 w-auto h-auto bg-white rounded-lg border m-6">
+    <div class="col-span-5">
+        <a href="#">
+            <img class="m-4 rounded-lg shadow-lg" src="https://blogger.googleusercontent.com/img/a/AVvXsEg2uY2NovHxvtPLB4PWGWowiZgGx79HUvRz3-2kcnYX8hbAvzsQhup8LjeAVybNwAu0SI59B8NlkUz_gHt59pir1GTMqEQRJsIDltXWZe0vKc9Wj91RgWqWB7yRbDjO0YE3qbGYg9vR6bUC-7nEbYPOWZcIQxvM7kW3vQh0gm81EuMPWqCEtmAqYWkjQQ=w640"/>
+        </a>
+    </div>
+    
+    <div class=" mx-4 col-span-5">
+        <a href="#">
+            <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        </a>
+        <p class=" font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        
+    </div>
+</div> -->
+
 
     </div>
   </div>
