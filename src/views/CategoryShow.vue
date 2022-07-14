751a3e7eeb5c2f99d3c0ac1527d63af2dcb10782
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-07-14 18:58:11 +0300

    add

---
+++ src/views/CategoryShow.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <CategoryCard />
+    </div>
+</template>
+
+<script>
+import CategoryCard from '@/components/Category/CategoryCard.vue';
+
+export default {
+    components: { CategoryCard },
+};
+</script>
