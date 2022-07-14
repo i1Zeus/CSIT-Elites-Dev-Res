42a3763d60d78c9eabb775b8f6832cfe05a538dc
Mosa Nameer <mosa.mn777@gmail.com>
2022-07-14 22:31:15 +0300

    add

--- src/views/ResShow.vue
+++ src/views/ResShow.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="grid grid-cols-12 bg-white h-screen">
-    <div class="col-span-7">
+<div class="bg-gray-200 p-16">
+    <div class="grid grid-cols-12 bg-white p-8 rounded-lg ">
+    <div class="col-span-6">
       <div class="flex flex-col">
-        <p class="font-semibold text-3xl ml-16 mt-10">
+        <p class="font-semibold text-4xl ml-16 mt-10">
             Front-End Source
         </p>
         <div class="mr-20">
@@ -14,7 +15,7 @@
             Jawi & Manar
           </p>
 
-          <p class="mt-4 ml-16">
+          <p class="mt-4 ml-16 text-lg">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae
             officia ea officiis qui laborum quisquam! Excepturi, sunt
             cupiditate! Repellat facere corporis delectus? Voluptates et
@@ -29,23 +30,25 @@
           <!-- <button class="bg-green-500 ml-16 mt-10 h-8 w-16 rounded hover:bg-green-600 hover:text-white">
             <span class="m-2 ">Visit</span>
           </button> -->
-            <ul class="list-disc mt-10 ml-16 ">
+            <ul class="list-disc mt-10 ml-16 text-lg">
                 <li>Link 1</li>
                 <li>Link 2</li>
                 <li>Link 3</li>
             </ul>
 
-            <button class="bg-green-500 ml-16 mt-10  h-8 w-16 rounded text-white hover:border hover:border-green-500 hover:bg-white hover:text-green-500">
-                <span class="m-2 ">Back</span>
+            <button class="bg-green-500 ml-16 mt-6  h-10 w-20 rounded text-white hover:border hover:border-green-500 hover:bg-white hover:text-green-500">
+                <span class="text-lg">Back</span>
             </button>
           
         </div>
       </div>
     </div>
-    <div class="col-span-5">
-        <div class="mr-10">
+    <div class="col-span-6 h-auto">
+        <div class="mr-10 mt-10 ">
             <img src="../assets/img/imgRes.png">
         </div>
     </div>
   </div>
+</div>
+  
 </template>
