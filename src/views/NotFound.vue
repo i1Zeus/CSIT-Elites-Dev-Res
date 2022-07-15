01c422c63385e206490e0f50b2e49816221a4937
Mosa Nameer <mosa.mn777@gmail.com>
2022-07-16 02:05:26 +0300

    add

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-        <h1> Not Found </h1>
-
-    </div>
-    
+  <div>
+    <h1 class="text-7xl text-center mt-64">Not Found</h1>
+  </div>
+  <div class="flex justify-center">
+    <GoBack class="ml-10 mt-14" />
+  </div>
 </template>
+
+
+<script>
+import GoBack from "../components/GoBack.vue";
+
+export default {
+    components: {
+        GoBack 
+    }
+}
+</script>
