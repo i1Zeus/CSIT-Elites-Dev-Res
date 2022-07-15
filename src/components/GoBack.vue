a91cac513a5bbfb7f704894fc4c3405b98f69283
Hussein Najah <husseinnajah123@gmail.com>
2022-07-15 13:25:19 +0300

    go back

---
+++ src/components/GoBack.vue
@@ -0,0 +1,7 @@
+<template>
+  <span
+    class="bg-green-500 flex justify-center font-medium ml-16 mt-6 h-10 w-20 rounded text-white hover:border hover:border-green-500 hover:bg-white hover:text-green-500"
+  >
+    <button @click="$router.back()">Go Back!</button>
+  </span>
+</template>
