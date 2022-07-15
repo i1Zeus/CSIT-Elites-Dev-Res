600550296c83807675be31290ad19d3f94fea0c9
Hussein Najah <husseinnajah123@gmail.com>
2022-07-15 22:29:39 +0300

    Update ResShow.vue

--- src/views/ResShow.vue
+++ src/views/ResShow.vue
@@ -1,15 +1,13 @@
 <template>
-<!-- title green same bold . delete cat title . go back component .center the items  -->
+  <!-- .center the items  -->
   <div class="h-screen flex items-center bg-gray-600 p-16">
     <div class="grid grid-cols-12 bg-white p-8 rounded-lg">
       <div class="col-span-6">
         <div class="flex flex-col">
-          <p class="font-semibold text-4xl ml-16 mt-10">Front-End Source</p>
+          <p class="font-semibold text-4xl ml-16 mt-10 text-primary-500">
+            {{ latestR.title }}
+          </p>
           <div class="mr-20">
-            <p class="text-green-500 text-3xl mt-10 ml-16">
-              {{ latestR.title }}
-            </p>
-
             <p class="text-green-500 ml-16">Jawi & Manar</p>
 
             <p class="mt-4 ml-16 text-lg">{{ latestR.discription }}</p>
@@ -21,13 +19,13 @@
               <li>Link 3</li>
             </ul>
 
-            <GoBack class="ml-10 mt-4" />
+            <GoBack class="ml-10 mt-14" />
           </div>
         </div>
       </div>
       <div class="col-span-6 h-auto">
-        <div class="mr-10 mt-10">
-          <img :src="latestR.img" />
+        <div >
+          <img class="relative rounded-md" :src="latestR.img" />
         </div>
       </div>
     </div>
@@ -35,8 +33,11 @@
 </template>
 <script>
 import getLatestR from "../composables/getLatestR";
-
+import GoBack from "../components/GoBack.vue";
 export default {
+  components: {
+    GoBack,
+  },
   props: ["id"],
   setup(props) {
     const { latestR, error, load } = getLatestR(props.id);
