509e37c06daf98d65beff3b1a098c6e8c9391f95
Ahmed <mr.darkboss1@gmail.com>
2022-07-15 08:13:54 +0300

    A

--- src/components/Category/CategoryCard.vue
+++ src/components/Category/CategoryCard.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="w-72 bg-white rounded-lg border border-secondary-300 shadow-md">
     <a href="#">
-      <img
-        class="rounded-lg w-72 h-36"
-        src="https://en.number13.de/content/images/2021/08/photo-1542831371-29b0f74f9713-2.jpg"
-        alt=""
-      />
+      <img class="rounded-lg w-72 h-36" :src="img" alt="" />
     </a>
     <div class="p-2 text-center">
       <h3 class="font-semibold">{{ title }}</h3>
 
       <div class="grid grid-cols-2">
-        <p class="text-gray-400 grid-col-1">{{ sub }}</p>
-        <p class="text-gray-400 grid-col-1">{{ res }}</p>
+        <p class="text-gray-400 grid-col-1">{{ sub }} Sub.</p>
+        <p class="text-gray-400 grid-col-1">{{ res }} Res.</p>
 
         <!-- <a href="#" class="grid-col-1">
                     <i class="fa-solid fa-angle-right"></i>
@@ -23,6 +19,6 @@
 </template>
 <script>
 export default {
-  props: ["title", "sub", "res"],
+  props: ["title", "sub", "res", "img"],
 };
 </script>
--- src/components/Category/CategoryShow.vue
+++ src/components/Category/CategoryShow.vue
@@ -16,6 +16,7 @@
           :title="category.title"
           :sub="category.sub"
           :res="category.res"
+          :img="category.img"
         />
       </div>
     </div>
