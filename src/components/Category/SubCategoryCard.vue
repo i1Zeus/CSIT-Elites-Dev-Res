27d5941beee4615189c4f7692fc169c363da8fef
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-07-15 11:45:42 +0300

    Update getSubCategory.js

--- src/components/Category/SubCategoryCard.vue
+++ src/components/Category/SubCategoryCard.vue
@@ -1,19 +1,29 @@
 <template>
-  <div class="w-72 h-72 bg-white rounded-lg border border-secondary-300 shadow-md">
-    <a href="#">
-      <img class="rounded-lg w-72 h-36" :src="img" alt="" />
-    </a>
-    <div class="p-2 text-center">
-      <h3 class="font-semibold">{{ title }}</h3>
+    <div class="w-72 h-72 bg-white rounded-lg border border-secondary-300 shadow-md">
+        <a href="#">
+            <img class="rounded-lg w-72 h-36" :src="img" alt="" />
+        </a>
+        <div class="p-2 text-center">
+            <h3 class="font-semibold">{{ SubCategory.title }}</h3>
 
-      <div class="grid grid-cols-2">
-        <p> {{description}} </p>
-      </div>
+            <div class="grid grid-cols-2">
+                <p> {{ SubCategory.description }} </p>
+                <p class="text-gray-400"> {{SubCategory.res}} Res </p>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
+
 <script>
+import getSubCategory from '../../composables/getSubCategory';
+
 export default {
-  
+    component: {},
+    setup() {
+        const { SubCategory, error, load } = getSubCategory();
+
+        load()
+        return { SubCategory, error }
+    },
 };
 </script>
