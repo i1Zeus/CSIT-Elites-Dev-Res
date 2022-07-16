d1dd86679494145682ca844e03703dd6616b9f0f
Hussein Najah <husseinnajah123@gmail.com>
2022-07-16 15:09:23 +0300

    Update NotFound.vue

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16"
+    class="flex lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16"
   >
     <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
       <div class="relative">
@@ -13,11 +13,13 @@
               Sorry about that! Please visit our hompage to get where you need
               to go.
             </p>
-            <button
-              class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50"
-            >
-              Take me there!
-            </button>
+            <router-link to="/">
+              <button
+                class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50"
+              >
+                Take me there!
+              </button>
+            </router-link>
           </div>
         </div>
         <div>
