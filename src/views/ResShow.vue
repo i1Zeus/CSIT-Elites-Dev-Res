0cc1d1aac4ef01e440fd65eabb717938d82819ca
Mosa Nameer <mosa.mn777@gmail.com>
2022-07-17 17:26:27 +0300

    add

--- src/views/ResShow.vue
+++ src/views/ResShow.vue
@@ -1,31 +1,30 @@
 <template>
   <!-- .center the items  -->
-  <div class="h-screen flex items-center bg-gray-600 p-16">
-    <div class="grid grid-cols-12 bg-white p-8 rounded-lg">
+  <div class="h-screen flex items-center p-16 bg-gray-600 ">
+    <div class="grid grid-cols-12 bg-white p-4 rounded-lg">
       <div class="col-span-6">
-        <div class="flex flex-col">
-          <p class="font-semibold text-4xl ml-16 mt-10 text-primary-500">
+        <div class="flex flex-col ml-12">
+          <p class="font-semibold text-4xl mt-10 text-primary-500">
             {{ latestR.title }}
           </p>
           <div class="mr-20">
-            <p class="text-green-500 ml-16">Jawi & Manar</p>
+            <p class="text-green-500 ">{{latestR.name}}</p>
 
-            <p class="mt-4 ml-16 text-lg">{{ latestR.discription }}</p>
+            <p class="mt-4  text-lg">{{ latestR.discription }}</p>
           </div>
           <div class="flex flex-col">
-            <ul class="list-disc mt-10 ml-16 text-lg">
+            <ul class="list-disc mt-10  text-lg">
               <li>Link 1</li>
               <li>Link 2</li>
               <li>Link 3</li>
             </ul>
           </div>
         </div>
-        <GoBack class="ml-10 mt-14" />
+        <GoBack class=" mt-14" />
       </div>
-
-      <div class="col-span-6 h-auto">
+      <div class="col-span-6 mt-4 mr-4">
         <div >
-          <img class="relative rounded-md" :src="latestR.img" />
+          <img class="relative rounded-md h-1/" :src="latestR.img" />
         </div>
 
         <div class="flex gap-5 mt-14 ">
