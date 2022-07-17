64888acebcd4fa979321b325e74db6e7bcec9055
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-07-17 17:10:47 +0300

    add

--- src/components/Category/SubCategoryCard.vue
+++ src/components/Category/SubCategoryCard.vue
@@ -1,14 +1,11 @@
 <template>
     <div class="w-72 bg-white rounded-lg border border-secondary-300 shadow-md">
-        <a href="#">
-            <img class="rounded-t-lg w-72 h-36" :src="img" alt="" />
-        </a>
-        <div class="p-2 text-center">
+            <a href="#">
+                <img class="rounded-t-lg w-72 h-36" :src="img" alt="" />
+            </a>
+        <div class="p-2 text-center grid grid-cols-2">
             <h3 class="font-semibold">{{ title }}</h3>
-
-            <div>
-                <p class="text-gray-400"> {{res}} Res </p>
-            </div>
+            <p class="text-gray-400"> {{ res }} Res </p>
         </div>
     </div>
 </template>
