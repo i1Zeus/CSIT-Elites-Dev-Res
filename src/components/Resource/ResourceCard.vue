a3c43d40dc773500b386b6127011bfe4ad04b7d2
Mosa Nameer <mosa.mn777@gmail.com>
2022-07-18 23:16:43 +0300

    add

--- src/components/Resource/ResourceCard.vue
+++ src/components/Resource/ResourceCard.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- card -->
 
-  <div class="bg-gray-50 rounded-lg border border-gray-200 shadow-md m-10 p-3">
+  <!-- <div class="bg-gray-50 rounded-lg border border-gray-200 shadow-md m-10 p-3">
     <div>
       <img class="rounded-t-lg bg-cover" src="https://www.aspfaqs.com/wp-content/uploads/2021/10/pankaj-patel-_SgRNwAVNKw-unsplash-1.jpg" alt="" />
     </div>
@@ -22,8 +22,38 @@
         <span class="">{{ publishDate }}</span>
       </div>
     </div>
-  </div>
+  </div> -->
 
+  <div class="wrapper bg-whiite antialiased text-gray-900 m-6 rounded">
+    <div>
+      <img
+        src="https://www.aspfaqs.com/wp-content/uploads/2021/10/pankaj-patel-_SgRNwAVNKw-unsplash-1.jpg"
+        alt=" random imgee"
+        class="w-full object-cover object-center rounded-lg shadow-md"
+      />
+      <div class="relative px-4 -mt-16">
+        <div class="bg-white p-5 rounded-lg shadow-lg">
+          <div class="mt-1">
+            <div class="text-lg font-semibold text-primary-500">
+              who to hack hassan k. in one sec using ja r rWJNJ
+            </div>
+            <div class="text-end mr-2">
+              <span>{{ publishDate }}</span>
+            </div>
+            <div class="flex gap-2">
+              <div v-for="tag in tags" :key="tag.id">
+                <span
+                  class="inline-block bg-gray-200 rounded-full w-auto h-auto p-2 text-xs font-semibold text-gray-700 "
+                >
+                  {{ tag.name }}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
