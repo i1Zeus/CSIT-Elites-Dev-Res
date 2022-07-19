3c1d1c185528922c1f7d26e2280e0dcc611468d9
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-07-19 18:40:57 +0300

    Update AddButton.vue

--- src/components/button/AddButton.vue
+++ src/components/button/AddButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     type="button"
-    class="text-white hover:text-green-700 border border-green-700 hover:bg-white bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 rounded-lg text-sm font-semibold px-4 py-2 text-center mr-2 mb-2"
+    class="text-white hover:text-primary-700 border border-primary-700 hover:bg-white bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-700 rounded-lg text-sm font-semibold px-4 py-2 text-center mr-2 mb-2"
   >
     <i class="fa-solid fa-plus"></i> {{ name }}
   </button>
