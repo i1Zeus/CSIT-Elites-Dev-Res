e8c5e44f5ddf53b71853ee68b8b14448f7568585
Ahmed <mr.darkboss1@gmail.com>
2022-07-19 14:35:31 +0300

    A

---
+++ src/components/button/AddButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    type="button"
+    class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+  >
+    + {{ name }}
+  </button>
+</template>
+<script>
+export default {
+  props: ["name"],
+};
+</script>
