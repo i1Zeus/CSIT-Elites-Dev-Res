c40b24ffda2fbcda3ee9f12147ca2529f238b23e
Ahmed <mr.darkboss1@gmail.com>
2022-07-20 13:32:45 +0300

    A

---
+++ src/composables/Home/getAllRes.js
@@ -0,0 +1,25 @@
+import { ref } from "vue";
+// ======> GET 12 Certified Resources <=====
+const getAllRes = () => {
+  const allRes = ref([]);
+  const errorAll = ref(null);
+
+  const loadRes = async () => {
+    try {
+      //  Suggest to change the url to get the data from the server to => http://127.0.0.1:8000/api/resources/getTwelveResources
+      let data = await fetch(
+        "http://127.0.0.1:8000/api/resources/getResourcesWithTags"
+      );
+      if (!data.ok) {
+        throw Error("no data here");
+      } else {
+        allRes.value = await data.json();
+      }
+    } catch (err) {
+      errorAll.value = err.message;
+      console.log(errorAll.value);
+    }
+  };
+  return { allRes, errorAll, loadRes };
+};
+export default getAllRes;
