fcbd0f06d0505dfc764b0a09f9bd34098fc6a656
Hussein Najah <husseinnajah123@gmail.com>
2022-07-21 17:11:06 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,16 +11,21 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 /* =======> Here We Add FontAwsome Icons <======= */
 import { faBookOpenReader } from "@fortawesome/free-solid-svg-icons";
-import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { faSquareXmark } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 
-
 /* =======> Remember to mention the newest icon in the Library <======= */
-library.add(faBookOpenReader, faUsers, faPen, faAngleRight, faSquareXmark , faXmark,faEllipsisVertical );
+library.add(
+  faBookOpenReader,
+  faPen,
+  faAngleRight,
+  faSquareXmark,
+  faXmark,
+  faEllipsisVertical,
+);
 
 const app = createApp(App);
 app.component("font-awesome-icon", FontAwesomeIcon);
