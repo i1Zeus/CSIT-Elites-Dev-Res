3f5832f9be6c61d87026a0610f33875ccf040389
Hussein Najah <husseinnajah123@gmail.com>
2022-07-21 21:31:16 +0300

    Update store.js

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     loggedIn: false,
-    user:null,
+    user: null,
     token: null,
   },
   mutations: {
@@ -20,10 +21,46 @@ export default new Vuex.Store({
       state.loggedIn = payload;
     },
   },
-  actions: {},
+  actions: {
+    preformeloginAction({ commit }, payload) {
+      return new Promise((resolve, reject) => {
+        axios
+          .post("http://127.0.0.1:8000/api/auth/login", {
+            email: payload.email,
+            password: payload.password,
+          })
+          .then((res) => {
+            commit("SET_token", res.data.access_token);
+            commit("SET_user", res.data.user);
+            commit("SET_loggedIn", true);
+            resolve(res);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    preformeLogoutAction({ commit, state }) {
+      return new Promise((resolve, reject) => {
+        axios
+          .post("http://127.0.0.1:8000/api/auth/logout",{
+            token: state.token,
+          })
+          .then((res) => {
+            commit("SET_token", null);
+            commit("SET_user", null);
+            commit("SET_loggedIn", false);
+            resolve(res);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+  },
   getters: {
     get_loggedIn(state) {
       return state.loggedIn;
-    }
-  }
+    },
+  },
 });
