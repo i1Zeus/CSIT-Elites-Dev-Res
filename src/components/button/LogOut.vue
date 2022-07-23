a1bf0cc05fd84d839c978a3b040296fe26253d37
Hussein Najah <husseinnajah123@gmail.com>
2022-07-23 19:03:21 +0300

    last patch

--- src/components/button/LogOut.vue
+++ src/components/button/LogOut.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     @click.prevent="preformeLogoutAction()"
-    class="text-white hover:text-primary-700 border border-primary-700 hover:bg-white bg-primary-700 focus:ring-1 focus:outline-none focus:ring-primary-700 rounded-lg text-md font-semibold px-4 py-2 text-center"
+    class="text-white hover:text-primary-700 border border-primary-700 hover:bg-white bg-primary-700 focus:ring-1 focus:outline-none focus:ring-primary-700 rounded-lg text-md font-semibold px-7 py-2 text-center"
   >
     <p>logout</p>
   </button>
