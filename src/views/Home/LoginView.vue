a047583f4d3cfcb03db9211bad1915b2319e0306
Hussein Najah <husseinnajah123@gmail.com>
2022-07-23 18:38:24 +0300

    some fixes

--- src/views/Home/LoginView.vue
+++ src/views/Home/LoginView.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="min-w-screen min-h-screen bg-gray-600 flex items-center justify-center px-5 py-5"
+    class="min-w-screen min-h-screen bg-gray-200 flex items-center justify-center px-5 py-5"
   >
     <div
       class="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden"
--- src/views/Resource/AddRec.vue
+++ src/views/Resource/AddRec.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen bg-gray-600 py-6 flex flex-col justify-center sm:py-12">
+  <div class="min-h-screen bg-gray-200 py-6 flex flex-col justify-center sm:py-12">
     <div class="relative py-3 sm:max-w-xl sm:mx-auto">
       <div class="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
         <form class="max-w-md mx-auto">
