493a8abf0b75231eea11ddbe590e58ba4ad5bac2
Hussein Najah <husseinnajah123@gmail.com>
2022-08-02 19:28:40 +0300

    Update CategoryShow.vue

--- src/components/Category/CategoryShow.vue
+++ src/components/Category/CategoryShow.vue
@@ -4,10 +4,9 @@
       <h1 class="text-left text-primary-700 text-2xl font-semibold mx-5 mt-5">
         Category
       </h1>
-      <AddCategory />
-    </div>
-    <div class="mt-4 md:mt-3">
-      <AddCategory />
+      <div class="mt-3 md:mt-3">
+        <AddCategory />
+      </div>
     </div>
     <div
       class="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-gray-100 rounded-lg mt-2 lg:mx-3 py-5 justify-center justify-items-center relative"
