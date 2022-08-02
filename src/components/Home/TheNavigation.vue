1f7f51d0fbfa780fe571b2493c443072f6453c03
Mosa Nameer <mosa.mn777@gmail.com>
2022-08-02 23:14:50 +0300

    add

--- src/components/Home/TheNavigation.vue
+++ src/components/Home/TheNavigation.vue
@@ -1,67 +1,66 @@
 <template>
   <div
-    class="fixed w-full z-50 bg-gray-100 md:py-3 px-6 md:flex justify-between items-center"
+    class="fixed w-full z-50 bg-gray-100 md:py-3 px-6 flex justify-between items-center"
   >
-    <div class="flex items-center font-bold">
+    <div class="flex order-2 items-center font-bold">
       <span
         class="text-primary-700 text-2xl md:text-3xl lg:text-4xl mt-3 md:mt-0 ml-9 md:ml-10"
       >
         Dev Resources
       </span>
     </div>
-    <div class="flex">
-      <div @click="showMenu = !showMenu" class="flex flex-col mt-5 md:hidden">
+
+    <div
+      @click="showMenu = !showMenu"
+      class="flex flex-col order-1 mt-5 md:hidden"
+    >
+      <button
+        type="button"
+        class="text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+      >
+        <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+          <path
+            fill-rule="evenodd"
+            d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+          ></path>
+        </svg>
+      </button>
+    </div>
+    <div class="flex flex-col order-1 mt-5 md:hidden">
+      <ul
+        :class="showMenu ? 'flex' : 'hidden'"
+        class="flex flex-col mt-5 space-y-4 mb-4 md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
+      >
+        <li
+          class="md:mx-4 font-semibold text-primary-600"
+          v-for="link in links"
+          :key="link.name"
+        >
+          <a :href="link.link" class="text-xl hover:text-primary-600">
+            {{ link.name }}
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="flex order-3">
+      <ul class="md:flex flex-wrap gap-2 font-semibold md:items-center hidden">
+        <li class="md:mx-4" v-for="link in links" :key="link.name">
+          <a :href="link.link" class="text-xl hover:text-primary-600">
+            {{ link.name }}
+          </a>
+        </li>
+      </ul>
+
+      <router-link class="mb-3 ml-1" to="/login">
         <button
-          type="button"
-          class="text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+          v-if="!loggedIn"
+          class="text-white hover:text-primary-700 border border-primary-700 hover:bg-white bg-primary-700 focus:ring-1 focus:outline-none focus:ring-primary-700 rounded-lg text-md font-semibold px-7 py-2 text-center"
         >
-          <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-            <path
-              fill-rule="evenodd"
-              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-            ></path>
-          </svg>
+          <p>Login</p>
         </button>
-
-        <ul
-          :class="showMenu ? 'flex' : 'hidden'"
-          class="flex-col mt-5 space-y-4 mb-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
-        >
-          <li
-            class="md:mx-4 font-semibold text-primary-600"
-            v-for="link in links"
-            :key="link.name"
-          >
-            <a :href="link.link" class="text-xl hover:text-primary-600">
-              {{ link.name }}
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="flex">
-        <ul class="md:flex flex-wrap gap-2 font-semibold md:items-center hidden">
-          <li
-            class="md:mx-4"
-            v-for="link in links"
-            :key="link.name"
-          >
-            <a :href="link.link" class="text-xl hover:text-primary-600">
-              {{ link.name }}
-            </a>
-          </li>
-        </ul>
-
-        <router-link class="mb-3 ml-1" to="/login">
-            <button
-              v-if="!loggedIn"
-              class="text-white hover:text-primary-700 border border-primary-700 hover:bg-white bg-primary-700 focus:ring-1 focus:outline-none focus:ring-primary-700 rounded-lg text-md font-semibold px-7 py-2 text-center"
-            >
-              <p>Login</p>
-            </button>
-            <LogOut class="md:mt-3 mt-4 " v-else />
-          </router-link>
-      </div>
+        <LogOut class="md:mt-3 mt-4" v-else />
+      </router-link>
     </div>
   </div>
 </template>
